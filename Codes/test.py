23ea545131dfa6ef151450d3e8acde4602b97b18
Chien02 <nguyentrinhtrongchien2019@gmail.com>
2025-11-01 01:30:48 +0700

    Windows now display background and Player
    - Added some lines of code to display background and character.
    - Changed size of the images (x4 bigger).

--- Codes/test.py
+++ Codes/test.py
@@ -1,13 +1,41 @@
 import pygame, sys
 
-from pygame.locals import *
+from pygame.locals import * # Import game's modules
+
+# ---- Variables ------
+# UI's properties
+game_title = "FA Simulation"
+SCALED_RATIO = 4
+max_width = 256 * SCALED_RATIO
+max_height = 192 * SCALED_RATIO
+window_size = (max_width, max_height)
+
+# UI's sprites
+background_picture = pygame.image.load("Assets/Images/Backgrounds/GrassBackgrounds.png")
+
+# Characters
+player_sprite_init_pos = (100, 100)
+player_sprite = pygame.image.load("Assets/Images/Characters/Rabbits/TempRabbit.png")
+
+# Systems or Mechanic or Game Loop
+clock = pygame.time.Clock()
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 400))
-pygame.display.set_caption('Hello World!')
+screen = pygame.display.set_mode(window_size, 0, 32)
+pygame.display.set_caption(f"{game_title}")
+
+
+# ----- Game Loop ------
 while True:
     for event in pygame.event.get():
+        # Mechanics go down here
+        screen.blit(background_picture, (0, 0))
+        screen.blit(player_sprite, player_sprite_init_pos)
+
         if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        pygame.display.update()
+            pygame.quit() # Stop pygame
+            sys.exit() # Stop script
+    
+    pygame.display.update()
+    clock.tick(30) # Maintain 30 fps
+    
